07bc1bbc26db20e0596bc14940b14d1253c00418
kangyonggan <java@kangyonggan.com>
2020-06-05 14:51:23 +0800

    9. 回文数

---
+++ src/com/kangyonggan/leetcode/_0009Solution.java
@@ -0,0 +1,50 @@
+package com.kangyonggan.leetcode;
+
+/**
+ * 9. 回文数
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ * <p>
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ * <p>
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ * @author kyg
+ */
+public class _0009Solution {
+
+    public static void main(String[] args) {
+        _0009Solution solution = new _0009Solution();
+
+        System.out.println(solution.isPalindrome(121));
+        System.out.println(solution.isPalindrome(-121));
+        System.out.println(solution.isPalindrome(10));
+        System.out.println(solution.isPalindrome(11));
+        System.out.println(solution.isPalindrome(1));
+        System.out.println(solution.isPalindrome(01));
+    }
+
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int halfLen = s.length() / 2;
+        for (int i = 0; i < halfLen; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
